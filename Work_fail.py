b485479be2d661c867b56cf2ea73c55f622aec69
Eugen Kobchuk <irravadi@gmail.com>
2023-04-23 11:50:35 +0300

    ex_7_done

--- Work_fail.py
+++ Work_fail.py
@@ -1,1 +1,12 @@
-
+try:
+    with open('MyDocument.txt', 'r') as file:
+        content = file.read()
+        old_str = input "Enter the string to be replaced "
+        new_str = "New line"
+        new_content = content.replace(old_str, new_str)
+    with open('MyDocument.txt', 'w') as file:
+        file.write(new_content)
+except FileNotFoundError:
+    print("Fail not found.")
+finally:
+    print("Done")
