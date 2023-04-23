494fb21379e42f76cc5dcfbad522b490c5eea770
Eugen Kobchuk <irravadi@gmail.com>
2023-04-23 11:27:27 +0300

    ex_5_done

--- Work_fail.py
+++ Work_fail.py
@@ -1,1 +1,16 @@
-
+try:
+    with open('MyDocument.txt', 'r') as file:
+        content = file.read()
+        words = content.split()
+        numbers = []
+        for word in words:
+            try:
+                number = float(word)
+                numbers.append(number)
+            except ValueError:
+                pass
+        print("The numbers contained in the file:", numbers)
+except FileNotFoundError:
+    print("Fail not found.")
+finally:
+    print("Done.")
