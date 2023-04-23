6aa05e53937e1dcfb0065959ef470a310fe51efd
Eugen Kobchuk <irravadi@gmail.com>
2023-04-23 10:55:33 +0300

    ex_3_done

--- Work_fail.py
+++ Work_fail.py
@@ -1,1 +1,10 @@
-
+try:
+    file = open('MyDocument.txt', 'r')
+    content = file.read()
+    words = content.split()
+    num_words = len(words)
+    print('Number of words on the screen:', num_words)
+except Exception as e:
+    print("Fail not found.")
+finally:
+    print("End programm.")
