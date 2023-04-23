7b3157b00de8cae3410d16d808687624e94cd7fb
Eugen Kobchuk <irravadi@gmail.com>
2023-04-23 11:38:34 +0300

    ex_6_done

--- Work_fail.py
+++ Work_fail.py
@@ -1,1 +1,15 @@
+try:
+    with open('MyDocument.txt', 'r') as file:
+        content = file.read()
+        lines = content.split('\n')
+        n = int(input("Enter quantity lines: "))
+        print("Last", n, "lines:")
+        for line in lines[-n:]:
+            print(line)
+except FileNotFoundError:
+    print('Fail not found')
+finally:
+    print("Done")
+
+
 
