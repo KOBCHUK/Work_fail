72a9ef70b7c1e27ebf0205352855bb90f1d681bb
Eugen Kobchuk <irravadi@gmail.com>
2023-04-23 11:20:51 +0300

    ex_4_done

--- Work_fail.py
+++ Work_fail.py
@@ -1,5 +1,13 @@
 try:
-    file = open('MyDocument.txt', 'r')
-    content = fail.read()
-
-except
+    with open('MyDocument.txt', 'r') as file:
+        content = file.read()
+        lines = content.split('\n')
+        search_word = input("Enter world for search: ")
+        matching_lines = [line for line in lines if search_word in line]
+        print("Lines containing the word '", search_word, "':", sep='')
+        for line in matching_lines:
+            print(line)
+except FileNotFoundError:
+    print("Fail not found.")
+finally:
+    print("Program finished.")
