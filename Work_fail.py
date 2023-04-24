36de3124664bafab8d8ac4398cc9bb97d0812ee9
Eugen Kobchuk <irravadi@gmail.com>
2023-04-24 20:39:50 +0300

    ex_3_done

--- Work_fail.py
+++ Work_fail.py
@@ -1,6 +1,6 @@
 try:
-    file = open('MyDocument.txt', 'r')
-    content = file.read()
+    _file = open('MyDocument.txt', 'r')
+    content = _file.read()
     words = content.split()
     num_words = len(words)
     print('Number of words on the screen:', num_words)
