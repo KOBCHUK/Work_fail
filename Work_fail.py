98afbc96e21ea210231444e1bfb24cd683cca679
Eugen Kobchuk <irravadi@gmail.com>
2023-04-27 19:19:49 +0300

    ex_9_done

--- Work_fail.py
+++ Work_fail.py
@@ -1,1 +1,13 @@
-
+try:
+    with open("MyDocument.txt", "r+") as f:
+        lines = f.readlines()
+        sorted_lines = sorted(lines)
+        f.seek(0)
+        f.truncate()
+        for line in sorted_lines:
+            f.write(line)
+except Exception as e:
+    print(e)
+    print("Error")
+finally:
+    print("Finally")
