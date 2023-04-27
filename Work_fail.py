82e235726aeae6a0f10cc1fa6b74686a725d5785
Eugen Kobchuk <irravadi@gmail.com>
2023-04-27 20:04:14 +0300

    ex_10_done

--- Work_fail.py
+++ Work_fail.py
@@ -1,1 +1,15 @@
-
+try:
+    line_number = int(input("Enter number the line: "))
+    with open("MyDocument.txt", "r+") as f:
+        lines = f.readlines()
+        print(lines[line_number - 1])
+        f.seek(0)
+        f.truncate()
+        for line in lines:
+            if line != lines[line_number - 1]:
+                f.write(line)
+except Exception as e:
+    print(e)
+    print("Error")
+finally:
+    print("Finally")
