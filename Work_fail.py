914b08adce2231622e5d7bf7afbf9d6eab212b75
Eugen Kobchuk <irravadi@gmail.com>
2023-04-27 18:54:26 +0300

    ex_8_done

--- Work_fail.py
+++ Work_fail.py
@@ -1,1 +1,14 @@
-
+try:
+    with open("MyDocument.txt", "r") as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        unique_lines = set(lines)
+        if len(lines) != len(unique_lines):
+            print("The file has duplicate lines.")
+        else:
+            print("There are no duplicate lines in the file.")
+except Exception as e:
+    print(e)
+    print("Error")
+finally:
+    print("Done")
